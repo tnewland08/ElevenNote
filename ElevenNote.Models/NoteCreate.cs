ec8e970f6ac7e3883b26ebade2cea907beec8d39
Todd Newland <tnewland08@gmail.com>
2020-04-23 21:04:38 -0400

    NoteCreate Model and NoteService

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage ="Please enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage ="Please use no more than 100 characters.")]
+        public string Title { get; set; }
+
+        [MaxLength(800)]
+        public string Content { get; set; }
+    }
+}
--- ElevenNote.Models/NoteListItem.cs
+++ ElevenNote.Models/NoteListItem.cs
@@ -5,25 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ElevenNote.Data
+namespace ElevenNote.Models
 {
-    public class Note
+    public class NoteListItem
     {
-        [Key]
         public int NoteId { get; set; }
-
-        [Required]
-        public Guid OwnerId { get; set; }
-
-        [Required]
         public string Title { get; set; }
 
-        [Required]
-        public string Content { get; set; }
-
-        [Required]
+        [Display(Name="Created")]
         public DateTimeOffset CreatedUtc { get; set; }
-
-        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
